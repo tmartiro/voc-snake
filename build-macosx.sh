600dd1b167318607e86ffd59e45bfbc9dac96e29
Tigran Martirosyan <tmartiro@gmail.com>
2024-04-06 18:38:31 +0400

    wasm32 - wip

--- build-macosx.sh
+++ build-macosx.sh
@@ -3,6 +3,7 @@
 set -xe
 voc=./voc/install/bin/voc
 voc_lib=./voc/install/lib/
+voc_include=./voc/install/C/include
 raylib=./raylib/src/libraylib.a
 
 compile_osx() {
@@ -16,6 +17,14 @@ compile_osx() {
 		-framework OpenGL -framework Cocoa -framework IOKit -framework CoreAudio -framework CoreVideo
 }
 
+compile_wasm() {
+	$voc -ce  Raylib.Mod
+	$voc -cm  Snake.Mod
+	clang -I${voc_include} --target=wasm32 -emit-llvm --no-standard-libraries -c -S Snake.c Raylib.c
+	llc -march=wasm32 -filetype=obj Snake.ll
+	llc -march=wasm32 -filetype=obj Raylib.ll
+	wasm-ld --no-entry --export=Snake_Init --export=Snake_Update --allow-undefined -o snake.wasm Snake.o Raylib.o
+}
 
 run() {
 	LD_LIBRARY_PATH=${voc_lib} DYLD_LIBRARY_PATH=${voc_lib} ./Snake
@@ -28,11 +37,14 @@ case $1 in
 	build)
 		compile_osx
 		;;
+	wasm)
+		compile_wasm
+		;;
 	example)
 		compile_example
 		;;
 	clean)
-		rm -rf *.o *.h *.c *.sym *.tmp*
+		rm -rf Snake *.o *.h *.c *.sym *.tmp* *.ll
 		;;
 	*)
 		compile_osx
